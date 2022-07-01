c36c25efa32620927be2f7aa0ed116278faea89a
1ilkwon <dlfrnjs318@gmail.com>
2022-07-01 18:33:21 +0900

    코딩 테스트 DAY4
    코딩테스트 준비 4일차
    solved.ac 브론즈5 문제풀이

--- Python_BaekJoon_Bronze5/2738.py
+++ Python_BaekJoon_Bronze5/2738.py
@@ -1,3 +1,9 @@
-a, b = list(map(int, input().split()))
-array = [input().split() for _ in range(b)]
-print(array)
+n, m = map(int, input().split())
+a, b = [], []
+for i in [a, b]:
+    for j in range(n):
+        i.append(list(map(int, input().split())))
+for i in range(n):
+    for j in range(m):
+        a[i][j] += b[i][j]
+    print(*a[i])
