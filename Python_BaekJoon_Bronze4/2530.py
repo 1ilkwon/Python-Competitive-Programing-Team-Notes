421a5508a90c413e935a3febb4a7adc98ebb2462
1ilkwon <dlfrnjs318@gmail.com>
2022-07-09 16:10:01 +0900

    코딩 테스트 DAY8
    코딩테스트 준비 8일차
    solved.ac 브론즈4 문제풀이

--- Python_BaekJoon_Bronze4/2530.py
+++ Python_BaekJoon_Bronze4/2530.py
@@ -1,2 +1,19 @@
 A, B, C = map(int, input().split())
 s = int(input())
+
+a = A*3600
+b = B*60
+total = a + b + C + s
+
+A = total // 3600
+B = (total - A*3600) // 60
+C = total - (A*3600) - (B*60)
+a2 = (A // 24)
+a1 = A - (a2*24)
+if A > 23:
+    print(a1, B, C, end='')
+else:
+    print(A, B, C, end='')
+    
+
+# 시 분 초를 이용하는 문제는 초단위로 환산하는 것을 생각해보기
---
+++ Python_BaekJoon_Bronze4/2752.py
@@ -0,0 +1,5 @@
+a = list(map(int, input().split()))
+a.sort()
+print(a[0],a[1],a[2])
+
+# 리스트 오름차순 정렬 : sort()
